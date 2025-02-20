3718adc25292b7c74fe50d64c55b64a9d0c2b35f
kynguyen <kybongdem@gmail.com>
2025-02-20 17:39:46 +0700

    update hang hoa

---
+++ ShopApp/Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApp.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShopApp/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Data;
+using ShopApp.ViewModels;
+
+namespace ShopApp.ViewComponents
+{
+    public class MenuLoaiViewComponent : ViewComponent
+    {
+        private readonly Hshop2023Context db;
+
+        public MenuLoaiViewComponent(Hshop2023Context context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var items = db.Loais.Select(lo => new MenuLoaiVM { 
+                MaLoai = lo.MaLoai,
+                TenLoai = lo.TenLoai,
+                SoLuong = lo.HangHoas.Count
+            });
+            return View(items); // Default cshtml
+            //return View("MenuLoai2", items); // Custom cshtml
+        }
+    }
+}
